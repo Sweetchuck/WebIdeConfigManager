b623cdcd794cb8e6bfca1ef966c2a5d0ffacd7f1
Andor <david-andor@kozpontiagy.hu>
2026-01-11 22:29:47 +0100

    2.x Upgrade sweetchuck/git-hooks to 3.x - .git-hooks.zsh

--- .git-hooks.zsh
+++ .git-hooks.zsh
@@ -1,31 +1,32 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-echo "BEGIN Git hook: ${sghHookName}"
+: "${sghHookName:?'argument is required'}"
+: "${sghHasInput:?'argument is required'}"
+
+echo 1>&2 "BEGIN Git hook: ${sghHookName}"
 
 function sghExit ()
 {
-    echo "END   Git hook: ${sghHookName}"
+	if [[ "${2}" != '' ]]; then
+		echo 1>&2 "${2}"
+	fi
 
-    exit $1
+	echo 1>&2 "END   Git hook: ${sghHookName}"
+
+	exit "${1}"
 }
 
-export COMPOSER_DISABLE_XDEBUG_WARN=1
-
-# @todo Better detection for executables: php, composer.phar.
-sghRobo="$(composer config 'bin-dir')/robo"
-
-test -s "${sghBridge}-local" && . "${sghBridge}-local"
-
-sghTask="githook:${sghHookName}"
+# @todo Better detection for executables: php, composer.phar and robo.
+robo="$(composer config 'bin-dir')/robo"
 
 # Exit without error if "robo" doesn't exists or it has no corresponding task.
-test -x "$sghRobo" || sghExit 0
-"${sghRobo}" help "${sghTask}" 1> /dev/null 2>&1 || sghExit 0
+test -x "${robo}" || sghExit 0 'robo executable not found'
+"${robo}" help "githook:${sghHookName}" 1> /dev/null 2>&1 || sghExit 0 'robo task does not exist.'
 
-if [ "$sghHasInput" = 'true' ]; then
-    "$sghRobo" "${sghTask}" $@ <<< $(</dev/stdin) || sghExit $?
+if [ "${sghHasInput}" = 'true' ]; then
+	"${robo}" "githook:${sghHookName}" "${@}" <&0 || sghExit $?
 else
-    "$sghRobo" "${sghTask}" $@ || sghExit $?
+	"${robo}" "githook:${sghHookName}" "${@}" || sghExit $?
 fi
 
 sghExit 0
